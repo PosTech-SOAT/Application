106ae8976a2d0d6bfd8a252dd2248c09796c09d0
Bruno Henrique <bruno.hsn6@gmail.com>
2024-01-29 00:14:43 -0300

    chore: merge k8s settings

---
+++ k8s/tests/load-test.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '100s', target: 100 }, // {Duração, Usuários virtuais}
+    { duration: '100s', target: 50 },
+  ],
+};
+
+export default function () {
+  let response = http.get('http://localhost:3000/api/clients');
+  
+  // Verifica se a resposta possui o status 200
+  check(response, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1); // Aguarda 1 segundo entre as requisições (pode ser ajustado conforme necessário)
+}
